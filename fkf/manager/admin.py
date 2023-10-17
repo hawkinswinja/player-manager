edc7aa54c940a35d97a7453bc106fde2fd5ad93a
hawkinswinja <hawkinswinja@gmail.com>
2023-10-18 01:44:49 +0300

    implemented the database models

--- fkf/manager/admin.py
+++ fkf/manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import County, Academy, Player
 
+admin.site.register(County)
+admin.site.register(Academy)
+admin.site.register(Player)
 # Register your models here.
--- fkf/manager/models.py
+++ fkf/manager/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
+class County(models.Model):
+    """model to store county objects"""
+
+    name = models.CharField(max_length=70, unique=True)
+    admin_name = models.CharField(max_length=70, null=False)
+    password = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return "{}, {}, {}".format(self.name, self.admin_name, self.password)
+
+
+class Academy(models.Model):
+    """Defines the Academy object"""
+
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
+    name = models.CharField(max_length=70, unique=True, null=False)
+    admin_name = models.CharField(max_length=70, null=False)
+    password = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return "{}, {}, {}, {}".format(
+            self.name, self.county, self.admin_name, self.password
+        )
+
+
+class Player(models.Model):
+    """Defines attributes of Player"""
+
+    pid = str(uuid4())
+    academy = models.ForeignKey(Academy, on_delete=models.CASCADE)
+    name = models.CharField(max_length=70, null=False)
+
+    def __str__(self):
+        return "{}, {}, {}".format(self.pid, self.academy, self.name)
